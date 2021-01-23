fb8c5b9c5f4c56593e68cf6e1cc45c8954a8bd9a
S.Shramko <shramko.web@yahoo.com>
2021-01-23 15:59:58 +0200

    chore: remove unused mock arrays

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -19,36 +19,6 @@ const FILE_CATEGORIES_PATH = path.resolve(__dirname, `../../../data/categories.t
 const FILE_NAME = path.resolve(__dirname, "../../../", "mocks.json");
 const MAX_DESCRIPTION_COUNT = 5;
 
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
-
 const OfferType = {
   OFFER: `offer`,
   SALE: `sale`,
@@ -69,7 +39,6 @@ const CategoryCountRestrict = {
   MIN: 1,
   MAX: 5,
 };
-
 
 const generateOffers = (count, {titles, sentences, categories, sumRestrict}) => {
   return [...Array(count)].map(() => {
