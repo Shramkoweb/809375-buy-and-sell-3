5e77cc46163a88077cc1ae9888de1f8bb7557b41
S.Shramko <shramko.web@yahoo.com>
2021-01-28 22:14:00 +0200

    chore: add pug templates rendering to app.js

--- src/express/app.js
+++ src/express/app.js
@@ -5,10 +5,15 @@ const express = require(`express`);
 const rootRouter = require(`./routes/root`);
 const myRouter = require(`./routes/my`);
 const offersRouter = require(`./routes/offers`);
+const path = require(`path`);
 
 const PORT = `3000`;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, `./public`)));
+app.set(`views`, path.resolve(__dirname, `./templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, rootRouter);
 app.use(`/my`, myRouter);
