40dea667e49597ae4da169f1fa38520cae12825f
S.Shramko <shramko.web@yahoo.com>
2021-01-30 22:36:52 +0200

    fix: add correct 500 err handling

--- src/express/app.js
+++ src/express/app.js
@@ -20,7 +20,12 @@ app.use(`/my`, myRouter);
 app.use(`/offers`, offersRouter);
 
 app.use((req, res) => res.status(400).render(`errors/400`));
-app.use((req, res) => res.status(500).render(`errors/500`));
+// didnt use next arg so need disable eslint
+// eslint-disable-next-line no-unused-vars
+app.use(((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render(`errors/500`);
+}));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
